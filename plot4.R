6b79cb36ba9b0faa7c978bed83b55f62d08829d2
sbartek <bartekskorulski@gmail.com>
2014-12-04 15:44:56 +0100

    finishes with plot 4

--- plot4.R
+++ plot4.R
@@ -2,5 +2,11 @@ if (!exists("epConsumption")) source("readEpConsumption.R")
 par(mfcol = c(2, 2))
 source("plot1.R")
 source("plot2.R")
-source("plot1.R")
-source("plot2.R")
+
+plot(epConsumption$Time, epConsumption$Voltage, type="l",
+     ylab = "Voltage", xlab = "datetime")
+
+plot(epConsumption$Time, epConsumption$Global_reactive_power, type="l",
+     ylab = "Global Active Power (kilowatt)", xlab = "datetime")
+
+
