b83e61e0f509c1562e98f9f4b670befde4ee5973
sbartek <bartekskorulski@gmail.com>
2014-12-04 15:13:19 +0100

    finishes plot3

--- cp1.R
+++ cp1.R
@@ -27,3 +27,9 @@ hist(epConsumption$Global_active_power, col = "red")
 plot(epConsumption$Time, epConsumption$Global_active_power, type="l",
      ylab = "Global Active Power (kilowatt)", xlab = "")
 
+#plot3
+plot(epConsumption$Time, epConsumption$Sub_metering_1, type="l",
+     xlab = "", ylab = "Energy sub metering")
+points(epConsumption$Time, epConsumption$Sub_metering_2, type="l", col="red")
+points(epConsumption$Time, epConsumption$Sub_metering_3, type="l", col="blue")
+legend("topright",legend=c("Sub_metering_1", "Sub_metering_1", "Sub_metering_1"),lty=c(1,1,1),col=c("black", "blue","red"))
